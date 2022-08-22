78654822335c8290d9e54a8127bdd44b61c48ce3
KyungJin Park <pkyungjin@pkyungjin-a01.vmware.com>
2022-08-22 17:34:14 +0900

    rollback to application1

--- src/main/java/com/example/hol/HolApplication.java
+++ src/main/java/com/example/hol/HolApplication.java
@@ -1,24 +1,23 @@
-package com.hol.demo;
+package com.example.hol;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
-import javax.servlet.http.*;
 import org.springframework.web.bind.annotation.*;
+
 
 @SpringBootApplication
 @RestController
-
 public class HolApplication {
 
-        @GetMapping("/")
-        public String sayHello(HttpServletRequest request) {
-                return "Hello from HOL team! your IP is: " + request.getRemoteAddr();
-        }
 
-    public static void main(String[] args) throws Exception {
+    @GetMapping("/")
+    String home() {
+        return "Hello from HOL team!";
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(HolApplication.class, args);
     }
 
 }
-
